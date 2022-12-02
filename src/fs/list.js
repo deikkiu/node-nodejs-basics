24ec52f899a38680374008aa5872dcf47e31095d
Timur Smagulov <deikku@mail.ru>
2022-12-02 21:48:26 +0600

    feat: completed list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = "node-nodejs-basics/src/fs/files";
+
+  fs.access(filePath, (err) => {
+    if (err) throw new Error("FS operation failed");
+    else {
+      fs.readdir(filePath, "utf-8", (err, data) => {
+        if (err) throw err;
+
+        console.log(data);
+      });
+    }
+  });
 };
 
 await list();
