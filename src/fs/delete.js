9451c52dbcef6a420fe8634ac8ed3810a2f1cd30
Timur Smagulov <deikku@mail.ru>
2022-12-02 20:59:33 +0600

    feat: completed delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  let filePath = "node-nodejs-basics/src/fs/files/fileToRemove.txt";
+
+  fs.access(filePath, (err) => {
+    if (err) throw new Error("FS operation failed");
+    else {
+      fs.unlink(filePath, (err) => {
+        if (err) throw err;
+
+        console.log("File deleted");
+      });
+    }
+  });
 };
 
 await remove();
