92703890354f025a8499978c40b61d19716affac
Timur Smagulov <deikku@mail.ru>
2022-12-02 21:11:29 +0600

    feat: completed read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  let filePath = "node-nodejs-basics/src/fs/files/fileToRead.txt";
+
+  fs.access(filePath, (err) => {
+    if (err) throw new Error("FS operation failed");
+    else {
+      fs.readFile(filePath, "utf-8", (err, data) => {
+        if (err) throw err;
+
+        console.log(data);
+      });
+    }
+  });
 };
 
 await read();
