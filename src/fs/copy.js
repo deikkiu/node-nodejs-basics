267752f69cf6931985cd3dcfc44c8f5a3421197d
Timur Smagulov <deikku@mail.ru>
2022-12-02 18:58:06 +0600

    feat: completed copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  const path = "node-nodejs-basics/src/fs";
+
+  fs.access(`${path}/files_copy`, fs.constants.F_OK, (err) => {
+    if (!err) throw new Error("FS operation failed");
+    else {
+      fs.mkdir(`${path}/files_copy`, { recursive: true }, (err) => {
+        if (err) throw err;
+
+        console.log("The folder was created");
+      });
+    }
+  });
+
+  fs.access(`${path}/files`, fs.constants.F_OK, (err) => {
+    if (err) throw new Error("FS operation failed");
+    else {
+      fs.cp(
+        `${path}/files`,
+        `${path}/files_copy`,
+        { recursive: true },
+        (err) => {
+          if (!err) console.error("Error");
+
+          console.log("The copy was completed");
+        }
+      );
+    }
+  });
 };
 
-copy();
+await copy();
