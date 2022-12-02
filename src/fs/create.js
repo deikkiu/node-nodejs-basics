f55ad055c89b8b3ae021779ee2e271fc09a4a42d
Timur Smagulov <deikku@mail.ru>
2022-12-02 18:56:29 +0600

    feat: completed create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const path = "node-nodejs-basics/src/fs/files/fresh.txt";
+
+  fs.access(path, fs.F_OK, (err) => {
+    if (!err) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.open(path, "w", (err) => {
+        if (err) throw err;
+
+        fs.writeFile(path, "I am fresh and young", "utf8", (err) => {
+          if (err) throw err;
+
+          console.log("File created");
+        });
+      });
+    }
+  });
 };
 
 await create();
