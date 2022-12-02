bbe0a5b506decfb8394f3658983cc15d7dcc5327
Timur Smagulov <deikku@mail.ru>
2022-12-02 21:27:15 +0600

    feat: completed rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  let filePath = "node-nodejs-basics/src/fs/files";
+
+  fs.access(filePath, (err) => {
+    if (err) throw new Error("FS operation failed");
+    else {
+      fs.rename(
+        `${filePath}/wrongFilename.txt`,
+        `${filePath}/properFilename.md`,
+        (err) => {
+          if (err) throw new Error("FS operation failed");
+
+          console.log("File was renamed");
+        }
+      );
+    }
+  });
 };
 
 await rename();
