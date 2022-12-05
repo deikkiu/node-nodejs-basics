e76595491c57750ce4822dfb0f88294a1aa1425d
Timur Smagulov <deikku@mail.ru>
2022-12-05 23:39:12 +0600

    feat: added main and worker functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const path = "node-nodejs-basics/src/wt/worker.js";
+  const worker = new Array(cpus().length).fill();
+
+  worker.map((acc, i) => {
+    new Worker(path), { workerData: i + 10 };
+  });
+
+  const workersRes = await Promise.allSettled(
+    worker.map(
+      (worker) =>
+        new Promise((resolve, reject) => {
+          worker.on("message", (data) => {
+            resolve({ status: "resolved", data });
+          });
+
+          worker.on("error", () => {
+            reject({ status: "error", data: null });
+          });
+        })
+    )
+  );
+
+  console.log(workersRes);
 };
 
 await performCalculations();
