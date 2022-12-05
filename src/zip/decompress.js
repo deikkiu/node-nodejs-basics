f5408ca2c6bd521c79fcef1fe593074eee0835be
Timur Smagulov <deikku@mail.ru>
2022-12-05 21:44:59 +0600

    feat: completed decompress unzip function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import zlib from "zlib";
+import fs from "fs";
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const path = "node-nodejs-basics/src/zip/files";
+  const gunzip = zlib.createGunzip();
+  const inpt = fs.createReadStream(`${path}/archive.gz`);
+  const out = fs.createWriteStream(`${path}/fileToCompress.txt`);
+
+  inpt.pipe(gunzip).pipe(out);
 };
 
 await decompress();
