357f007ce54ea858f8eb5769a417ed424a73c042
Timur Smagulov <deikku@mail.ru>
2022-12-05 21:23:36 +0600

    feat: completed args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+import { argv } from "process";
+
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const argument = argv.reduce((acc, el, i, arr) => {
+    if (el.startsWith("--")) return [...acc, `${el.slice(2)} is ${arr[i + 1]}`];
+    else return acc;
+  }, []);
+
+  console.log(argument);
 };
 
 parseArgs();
