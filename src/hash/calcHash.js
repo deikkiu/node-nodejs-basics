7f5a73dd2e75c8de5a36eec9d372531e2f60978e
Timur Smagulov <deikku@mail.ru>
2022-12-05 18:19:03 +0600

    feat: implemented a function that calculates the SHA256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const path = "node-nodejs-basics/src/hash/files/fileToCalculateHashFor.txt";
+
+  fs.readFile(path, "utf-8", (err, data) => {
+    if (err) throw err;
+
+    const hash = crypto.createHash("sha256").update(data).digest("hex");
+
+    console.log(hash);
+  });
 };
 
 await calculateHash();
