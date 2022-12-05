3ba30e54550113b49e6df7e45d6412c7a7a35491
Timur Smagulov <deikku@mail.ru>
2022-12-05 22:19:52 +0600

    feat: completed transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  // Write your code here
+  const transform = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, String(chunk).replace(" ", ""));
+    },
+  });
+
+  stdin.pipe(transform).pipe(stdout);
 };
 
 await transform();
