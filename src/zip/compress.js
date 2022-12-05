bcfa7425b63693902469b57a756706a79afe79c9
Timur Smagulov <deikku@mail.ru>
2022-12-05 21:38:30 +0600

    feat: added compress function file to zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import zlib from "zlib";
+import fs from "fs";
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const path = "node-nodejs-basics/src/zip/files";
+  const gzip = zlib.createGzip();
+  const inpt = fs.createReadStream(`${path}/fileToCompress.txt`);
+  const out = fs.createWriteStream(`${path}/archive.gz`);
+
+  inpt.pipe(gzip).pipe(out);
 };
 
 await compress();
