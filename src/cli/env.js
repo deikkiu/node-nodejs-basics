24cf5fa89d318ed0553ca2449254766695bc6d3d
Timur Smagulov <deikku@mail.ru>
2022-12-05 21:16:32 +0600

    feat: added env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,20 @@
+import { env } from "process";
+
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const pref = "RSS_";
+  const rssInclude = Object.entries(env);
+
+  rssInclude
+    .filter((el) => {
+      el[0].includes(pref);
+    })
+    .map((el) => {
+      el.join("=");
+    })
+    .join(";");
+
+  console.log(rssInclude);
 };
 
 parseEnv();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+import process from "process";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const path = "node-nodejs-basics/src/streams/files/fileToRead.txt";
+  const readable = fs.createReadStream(path, { highWaterMark: 9 });
+
+  readable.on("data", (chunk) => {
+    console.log(String(chunk));
+  });
 };
 
 await read();
